196542b874ae171f81566a1f88f783080169baee
GCTEDT <gcenni@toku-e.com>
2025-09-16 13:04:34 +0200

    #1 OdooExperience Day1 Chapter 3-5

---
+++ estate/model/Test_Model.py
@@ -0,0 +1,24 @@
+from odoo import models, fields, tools, api
+
+class TestModel(models.Model):
+    _name = "test.model"
+    _description = "Test Model"
+
+    name = fields.Char(
+        required=True, 
+        index=True)
+    description = fields.Text()
+    postcode = fields.Char()
+    date_availability = fields.Date()
+    expected_price = fields.Float()
+    selling_price = fields.Float()
+    bedrooms = fields.Integer()
+    living_area = fields.Integer()
+    facades = fields.Integer()
+    garage = fields.Boolean()
+    garden = fields.Boolean()
+    garden_area = fields.Integer()
+    garden_orientation = fields.Selection(
+        string='GardenOrientationCustomLabel',
+        selection=[('north','North'), ('south','South'), ('east' ,'East'), ('west','West')],
+        help='Is this a tooltip?')
