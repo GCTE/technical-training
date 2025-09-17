e336988f818e6af751c4ff7a5944367d992da8e7
GCTEDT <gcenni@toku-e.com>
2025-09-17 15:54:59 +0200

    #1 OE211 inline view

--- estate/model/Estate_Property_Type_Inline.py
+++ estate/model/Estate_Property_Type_Inline.py
@@ -10,6 +10,6 @@ class EstatePropertyTypeInLine(models.Model):
     
     property_type_id = fields.Many2one("estate.property.type", string="Property Type")
 
-    field_1 = fields.Char()
-    field_2 = fields.Char()
-    field_3 = fields.Char()
+    # field_1 = fields.Char()
+    # field_2 = fields.Char()
+    # field_3 = fields.Char()
