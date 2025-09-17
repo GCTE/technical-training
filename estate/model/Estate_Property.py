6c30e6635b55d72d47cba4884f59323560c58abe
GCTEDT <gcenni@toku-e.com>
2025-09-17 11:31:10 +0200

    #1 OE207 offer view

--- estate/model/Estate_Property.py
+++ estate/model/Estate_Property.py
@@ -56,3 +56,26 @@ class EstateProperty(models.Model):
     buyer_id = fields.Many2one("res.partner", copy=False)
     tag_ids = fields.Many2many("estate.property.tag")
     offer_ids = fields.One2many("estate.property.offer", "property_id")
+
+    total_area = fields.Float(
+        readonly=True,
+        compute="_compute_total_area"
+    )
+
+    best_price = fields.Float(
+        readonly=True,
+        compute="_compute_best_price"
+    )
+
+    @api.depends('living_area', 'garden_area')
+    def _compute_total_area(self):
+        for property in self:
+            property.total_area = property.living_area + property.garden_area
+
+    @api.depends('offer_ids.price')
+    def _compute_best_price(self):
+        for property in self:
+            if property.offer_ids:
+                property.best_price = max(property.offer_ids.mapped('price'), default=0.0)
+            else:
+                property.best_price = 0.0
